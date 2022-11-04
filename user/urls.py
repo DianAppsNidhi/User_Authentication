dfcf66b35934630d8d36206dd0012b0caa7c1a87
DianAppsNidhi <nidhi.kaushik@dianapps.com>
2022-11-04 10:30:05 +0530

    Initial commit

---
+++ user/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+# from knox.views import LoginView as KnoxLoginView
+# from rest_framework import generics, permissions
+# from rest_framework.authtoken.serializers import AuthTokenSerializer
+# from knox.models import AuthToken
+
+from user import views
+ 
+urlpatterns = [
+   
+    path('signup/', views.UserAPI.as_view()),
+    path('login/', views.UserLoginAPI.as_view())
+]    
+
